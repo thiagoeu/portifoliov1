d8ab61a493263d721967e722bc47a994b01057ae
thiagoeu <araujo.thiago1051@gmail.com>
2025-07-11 07:47:01 -0300

    refact: ajuste de links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { IoLogoInstagram, IoLogoWhatsapp } from "react-icons/io";
 import { IoLogoLinkedin } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -11,30 +12,29 @@ const Footer = () => {
           &copy; {new Date().getFullYear()} Todos os direitos reservados.
         </p>
         <div className="flex gap-6">
-          <a
-            href="..."
+          <Link
+            to={"https://www.instagram.com/thiagoeu"}
             target="_blank"
-            rel="noopener noreferrer"
             className="transition hover:text-pink-400"
           >
             <IoLogoInstagram size={22} />
-          </a>
-          <a
-            href="..."
+          </Link>
+          <Link
+            to={"https://www.linkedin.com/in/thiago-araujo-946183227/"}
             target="_blank"
-            rel="noopener noreferrer"
             className="transition hover:text-blue-400"
           >
             <IoLogoLinkedin size={22} />
-          </a>
-          <a
-            href="..."
+          </Link>
+          <Link
+            to={
+              "https://wa.me/5583996485508?text=Gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os"
+            }
             target="_blank"
-            rel="noopener noreferrer"
             className="transition hover:text-green-400"
           >
             <IoLogoWhatsapp size={22} />
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
